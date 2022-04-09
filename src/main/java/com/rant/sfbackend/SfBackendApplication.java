e1d2b77094f91bbff5e84f17bf3831cf6f38ad7a
Timur <iostream.timur@gmail.com>
2022-04-09 17:03:51 +0600

    Initial commit

---
+++ src/main/java/com/rant/sfbackend/SfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rant.sfbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rant/sfbackend/SfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rant.sfbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
